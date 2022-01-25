bee7e17624f3261d109cbb4b66de5a6309d82bca
kpotoh <axepon@mail.ru>
2022-01-25 22:09:43 +0200

    add converter 192>96 & sample sigProfiler output

--- src/9.Mut_signature_extraction.py
+++ src/9.Mut_signature_extraction.py
@@ -6,9 +6,12 @@ def main_function():
     # genInstall.install('GRCh37')
 
     data = sig.importdata("matrix")
+    print(data)
     sig.sigProfilerExtractor(
-        "matrix", "../data/example_sig_output", 
-        data, minimum_signatures=1, maximum_signatures=3)
+        "matrix", "./data/example_sig_output_cpu", 
+        data, minimum_signatures=1, maximum_signatures=3,
+        gpu=False, cpu=-1
+    )
 
 
 if __name__ == "__main__":
