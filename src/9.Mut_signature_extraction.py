bcfbc7f46467e394a7eccfb67d049ebe9d4636cf
kpotoh <axepon@mail.ru>
2022-01-26 18:53:46 +0200

    output updated

--- src/9.Mut_signature_extraction.py
+++ src/9.Mut_signature_extraction.py
@@ -1,18 +1,22 @@
-from SigProfilerMatrixGenerator import install as genInstall
 from SigProfilerExtractor import sigpro as sig
+import click
 
 
-def main_function():
-    # genInstall.install('GRCh37')
-
-    data = sig.importdata("matrix")
-    print(data)
+@click.command("extractor", help="Pass path to mutspec table in SigProfiler format and path to output directory. Script release signatures from mutspec and decompose it with COSMIC database")
+@click.argument("input_mutspec", required=True, type=click.Path(exists=True))
+@click.argument("outdir", required=True, type=click.Path(exists=False))
+@click.option("-m", "--max_signatures", default=5, show_default=True, type=int, help="maximum number of signatures to release")
+@click.option("-t", "--threads", default=-1, show_default=True, type=int, help="number of threads to use")
+def main(input_mutspec, outdir, max_signatures, threads):
     sig.sigProfilerExtractor(
-        "matrix", "./data/example_sig_output_cpu", 
-        data, minimum_signatures=1, maximum_signatures=3,
-        gpu=False, cpu=-1
+        "matrix", outdir,
+        input_mutspec,
+        minimum_signatures=1,
+        maximum_signatures=max_signatures,
+        cpu=threads,
+        gpu=False,
     )
 
 
 if __name__ == "__main__":
-    main_function()
+    main()
